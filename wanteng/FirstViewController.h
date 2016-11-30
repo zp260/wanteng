48454f3794a577143d98d9404c7e34cca96d4a49
mrz <17966059@qq.com>
2016-11-30 15:00:44 +0800

    init with first view

--- wanteng/FirstViewController.h
+++ wanteng/FirstViewController.h
@@ -8,8 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+@interface FirstViewController : UIViewController <UITableViewDelegate,UITableViewDataSource,UIScrollViewDelegate>
 
+@property (strong, nonatomic) IBOutlet UIScrollView *scroolView;
 
+//8个按钮
+@property (strong, nonatomic) IBOutlet UIButton *newsButton;
+@property (strong, nonatomic) IBOutlet UIButton *infoButton;
+@property (strong, nonatomic) IBOutlet UIButton *yjManageButton;
+@property (strong, nonatomic) IBOutlet UIButton *govFilesButton;
+@property (strong, nonatomic) IBOutlet UIButton *sangongButton;
+@property (strong, nonatomic) IBOutlet UIButton *jingjiButton;
+@property (strong, nonatomic) IBOutlet UIButton *vipOrgButton;
+@property (strong, nonatomic) IBOutlet UIButton *cityManageButton;
+@property (strong, nonatomic) IBOutlet UIButton *unitManageButton;
+@property (strong, nonatomic) IBOutlet UIButton *messageButton;
+
+//4个按钮
+@property (strong, nonatomic) IBOutlet UIButton *historyButton;
+@property (strong, nonatomic) IBOutlet UIButton *adminAreaButton;
+@property (strong, nonatomic) IBOutlet UIButton *nationButton;
+@property (strong, nonatomic) IBOutlet UIButton *areaButton;
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@property (strong,nonatomic) NSArray *recipes;
 @end
 
--- wanteng/FirstViewController.m
+++ wanteng/FirstViewController.m
@@ -17,9 +17,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _recipes = [NSArray arrayWithObjects:@"第一条数据",@"第二条数据",@"第三条数据",@"第四条数据",@"第五条数据",@"第一条数据",@"第二条数据",@"第三条数据",@"第四条数据",@"第五条数据", nil];
 }
 
-
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_recipes count];
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"listCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    cell.textLabel.text = [_recipes objectAtIndex:indexPath.row];
+    return cell;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
